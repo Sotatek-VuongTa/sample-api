dc1a4dcc4088199e6901c8fb7298eedbf574a24c
AnNguyen <vuong.ta@sotatek.com>
2025-04-22 17:48:54 +0700

    feat: init source

---
+++ src/main/java/com/musina/api/MusinaApplication.java
@@ -0,0 +1,13 @@
+package com.musina.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusinaApplication.class, args);
+	}
+
+}
